8bbacca3f4709535a31b2ae3d81afb030bec1eb3
nickgenov <nick.genov@hotmail.com>
2015-08-14 18:42:29 +0300

    Finished BitArray

---
+++ 02.Static-Members-and-Namespaces/Homework/BitArrayToDecimal/BitArray.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Numerics;
+
+namespace BitArrayToDecimal
+{
+    public class BitArray
+    {
+        private int bitArraySize;
+
+        public BitArray(int size)
+        {
+            this.BitArraySize = size;
+            this.Bits = new byte[BitArraySize];
+        }
+
+        public int BitArraySize
+        {
+            get
+            {
+                return this.bitArraySize;
+            }
+            set
+            {
+                if (value < 1 || value > 100000)
+                {
+                    throw new ArgumentOutOfRangeException("BitArraySize", "BitArraySize must be in the range [1...100000].");
+                }
+
+                this.bitArraySize = value;
+            }
+        }
+
+        private byte[] Bits { get; set; }
+
+        public byte this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.BitArraySize )
+                {
+                    throw new ArgumentOutOfRangeException("Index", "Index out of range.");
+                }
+
+                return Bits[this.BitArraySize - 1 - index];
+            }
+            set
+            {
+                if (value > 1)
+                {
+                    throw new ArgumentOutOfRangeException("Bit", "Bits must be either 1 or 0.");
+                }
+
+                Bits[this.BitArraySize - 1 - index] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            BigInteger decimalNum = ConvertBitArrayToDecimal();
+            return decimalNum.ToString();
+        }
+
+        private BigInteger ConvertBitArrayToDecimal()
+        {
+            BigInteger power = 0;
+            BigInteger decimalNum = 0;
+
+            for (int i = this.Bits.Length - 1; i >= 0; i--)
+            {
+                if (Bits[i] == 1)
+                {
+                    decimalNum += RaiseTwoToPower(power);
+                }
+                power++;
+            }
+
+            return decimalNum;
+        }
+
+        private BigInteger RaiseTwoToPower(BigInteger power)
+        {
+            BigInteger result = 1;
+            for (int i = 1; i <= power; i++)
+            {
+                result *= 2;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 02.Static-Members-and-Namespaces/Homework/BitArrayToDecimal/BitArrayMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BitArrayToDecimal
+{
+    class BitArrayMain
+    {
+        static void Main()
+        {
+            var num = new BitArray(100000);
+
+            // 1111 1111 is 255
+            for (int i = 0; i < 8; i++)
+            {
+                num[i] = 1;
+            }
+            Console.WriteLine(num); // 255
+
+            num[99999] = 1; // set the biggest bit to 1, it is a huge number!
+            Console.WriteLine(num);
+        }
+    }
+}
